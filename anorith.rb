46326686face4342f472914932355aa6485bb891
Chris <chris.haack@me.com>
2013-11-20 18:12:28 -0500

    update pokemon files with methods

--- anorith.rb
+++ anorith.rb
@@ -2,18 +2,32 @@ require 'awesome_print'
 
 class Anorith
 
+#Fill this with magic tomorrow
+	type_effectiveness = {
+		{:Damaged_normally_by => []}
+		{:Weak_to => []}
+		{:Immune_to => []}
+		{:Resistant_to => []}
+	}
 
-	type_effectiveness = 
-
-	def initialize
-		@name = "weedle"
+	def initialize(name)
+		@type = ["Rock", "Bug"]
+		@catch_rate = "45 (5.9%)"
+		@entry = "Anorith, the Old Shrimp Pokémon. Anorith once inhabited old seas, and is believed to have used its two well developed hooves to capture prey."
+		@hp = 45
+		@exp = 119
+		@name = name
+		@level = level_set
 		@leveling_rate = "Erratic"
-		@level = rand(6) + 1
-		@height = .3m
+		@height = "0.7m"
 		@state = "unevolved"
-		@weight = "7.1lbs"
-		@egg_group = "Bug"
-		@abilities = ["Battle_armor", "swift_swim"]
+		@weight = "27.6 1lbs"
+		@egg_group = "Water 3"
+		@abilities = ["Battle Armor", "Swift Swim"]
+	end
+
+	def level_set
+		rand(6) + 1
 	end
 
 	def level_up
@@ -29,3 +43,12 @@ class Anorith
   def say_name
     "Anorith!"
   end
+
+end
+
+anorith1 = Anorith.new
+ap anorith1.run_away
+ap anorith1.evolve(7)
+ap anorith1.level
+
+
--- dwebble.rb
+++ dwebble.rb
@@ -2,15 +2,38 @@ require 'awesome_print'
 
 class Dwebble
 
-	def initialize
-		@name = "weedle"
-		@leveling_rate = "Erratic"
+	def initialize(name)
+		@type = ["Bug", "Rock"] 
+		@catch_rate = "190 (24.8%)"
+		@entry = "Dwebble, the Rock Inn Pokémon. After finding a suitable rock, Dwebble digs a hole in the rock's bottom, to use as a protective shell. Dwebble can make rock easier to carve by producing a liquid from its mouth."
+		@hp = 50
+		@exp = 65
+		@name = name
+		@leveling_rate = "Medium Fast"
 		@level = rand(6) + 1
-		@height = .3m
+		@height = "0.3m"
 		@state = "unevolved"
-		@weight = "7.1lbs"
-		@egg_group = "Bug"
+		@weight = "32 lbs."
+		@egg_group = ["Bug", "Mineral"]
 		@abilities = ["Battle_armor", "swift_swim"]
 	end
-	
+
+	def level_set
+		rand(34) + 1
+	end
+
+	def level_up
+    if exp > level**3
+      level += 1
+    end
+  end
+
+  def gain_exp new_exp
+    exp += new_exp
+  end
+
+  def say_name
+    "Dwebble!"
+  end
+
 end
